ea19811418a5c71612bdf5fc397fd5d1440415c0
VayamRakshamah <vinaygupta890@gmail.com>
2020-10-20 08:40:32 +0530

    final change

--- python.py
+++ python.py
@@ -163,7 +163,7 @@ if __name__ == "__main__":
 					command=lambda: press('.'), height=1, width=7) 
 	Decimal.grid(row=6, column=0) 
 	# start the GUI 
-	gui.mainloop() 
+		gui.mainloop() 
 	
 	# The code is finished 
 	# Further improvements to be made  
